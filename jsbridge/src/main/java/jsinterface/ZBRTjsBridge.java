fdb0c8118e02b28dcacc8340ab3423c21f7f2bc2
wlj1986 <190785060@qq.com>
2019-02-14 10:43:01 +0800

    添加jssdk

---
+++ jsbridge/src/main/java/jsinterface/ZBRTjsBridge.java
@@ -0,0 +1,10 @@
+package jsinterface;
+
+/**
+ * 浙报集团通用JSSDK
+ * Created by wanglinjie.
+ * create time:2019/2/14  上午10:32
+ */
+public class ZBRTjsBridge {
+    public static final String PREFIX_JS_METHOD_NAME = "ZBJTJSBridge";
+}
